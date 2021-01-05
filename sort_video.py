a362d3714994aae6247fdd5c9c114847252f5109
Jason <jconstam@gmail.com>
2021-01-04 17:48:29 -0700

    Added command line arguments

--- sort_video.py
+++ sort_video.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+import argparse
 
 from cv2 import VideoWriter, VideoWriter_fourcc
 
@@ -13,6 +14,24 @@ from sorters.insertion import insertion_sort
 from sorters.selection import selection_sort
 from sorters.bubble import bubble_sort
 from sorters.merge import merge_sort
+
+INSERT_NAME = 'Insertion'
+SELECT_NAME = 'Selection'
+BUBBLE_NAME = 'Bubble'
+MERGE_NAME = 'Merge'
+
+short_names = {
+    INSERT_NAME: ['ins', 'insert', 'insertion'],
+    SELECT_NAME: ['sel', 'select', 'selection'],
+    BUBBLE_NAME: ['bub', 'bubble'],
+    MERGE_NAME: ['merge']
+}
+alg_classes = {
+    INSERT_NAME: insertion_sort,
+    SELECT_NAME: selection_sort,
+    BUBBLE_NAME: bubble_sort,
+    MERGE_NAME: merge_sort
+}
 
 
 def make_video(folder: str, sorter: sort_base, rand_data: list) -> None:
@@ -37,9 +56,27 @@ def make_video(folder: str, sorter: sort_base, rand_data: list) -> None:
     store.convert(raw_file_name, final_file_name)
 
 if __name__ == '__main__':
-    size = 1000
-    folder = 'output'
-    rand_data = random.sample(range(0, size), size)
-    sorters = [insertion_sort(), selection_sort(), bubble_sort(), merge_sort()]
+    parser = argparse.ArgumentParser(description='Generate videos of sorting algorithms in action')
+    parser.add_argument('-s', '--size', type=int, default=1000, help='The size of the array to be sorted')
+    parser.add_argument('-o', '--outputFolder', type=str, default='output/', help='The path to put the videos')
+    parser.add_argument('algorithms', metavar='alg', type=str, nargs='+', help='Sorting algorithms to run')
+    args = parser.parse_args()
+
+    folder = os.path.abspath(args.outputFolder)
+    assert os.path.exists(folder), 'Output path does not exist'
+
+    print('Sorting arrays of {} elements'.format(args.size))
+    print('Output will be located in "{}"'.format(folder))
+    sorters = []
+    for alg in args.algorithms:
+        for key, value in short_names.items():
+            if alg.lower() in value:
+                print('Using sort algorithm "{}"'.format(key))
+                sorters.append(alg_classes[key]())
+                break
+        else:
+            assert 0 == 1, 'Algorithm {} not found'.format(alg)
+
+    rand_data = random.sample(range(0, args.size), args.size)
     for sorter in sorters:
         make_video(folder, sorter, rand_data.copy())
