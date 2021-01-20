85574895858a87dbad39c4cf3d2a62777eaf1b75
Jason Constam <callmebob@gmail.com>
2021-01-19 21:44:39 -0700

    Changes to github actions (#4)
    * Changes to github actions

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -7,7 +7,7 @@ echo "Previous version: ${LAST}"
 echo "New version:      ${NEXT}"
 
 echo "Creating tag..."
-git tag -a ${NEXT} -m "Version ${NEXT}"
+echo "git tag -a ${NEXT} -m \"Version ${NEXT}\""
 
 echo "Pushing tag..."
-git push origin ${NEXT}
+echo "git push origin ${NEXT}"
