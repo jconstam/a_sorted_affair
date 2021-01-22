d3504f7efb5772f3c1edec571be34a6fe3b83826
Jason <callmebob@gmail.com>
2021-01-21 20:05:06 -0700

    Added more common tests

--- tests/test_insert.py
+++ tests/test_insert.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python3
+
 from . import common
 from sorters.insertion import insertion_sort
 
 def test__name():
     assert insertion_sort().name() == 'Insertion'
+
+def test__sort__empty():
+    common.test__sort__empty(insertion_sort())
+
+def test__sort__singleton():
+    common.test__sort__singleton(insertion_sort())
 
 def test__sort__presorted():
     common.test__sort__presorted(insertion_sort())
@@ -12,3 +20,6 @@ def test__sort__reverse_sorted():
 
 def test__sort__random():
     common.test__sort__random(insertion_sort())
+
+def test__sort__random_big():
+    common.test__sort__random_big(insertion_sort())
