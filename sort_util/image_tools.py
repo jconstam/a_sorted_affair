31d2c4ae1c32ef35660680dcfdb409455ee50fc0
Jason <callmebob@gmail.com>
2021-01-22 21:23:00 -0700

    Fixed sizing of bars to be smooth Initial video is as wide as needed, then converted to the desired size

--- sort_util/image_tools.py
+++ sort_util/image_tools.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageFont
 class draw_image:
     def __init__(self, width, height, size) -> None:
         self.size = size
-        self.bar_width = int(width / size)
+        self.bar_width = max(round(width / size), 1)
         self.bar_height_scaler = height / size
         self.width = self.bar_width * self.size
         self.height = height
@@ -27,6 +27,7 @@ class draw_image:
     def __get_color(self, value: int):
         vectors = [
             (0, 0, 255),
+            (0, 64, 255),
             (0, 255, 255),
             (0, 255, 0),
             (255, 255, 0),
@@ -60,12 +61,12 @@ class draw_image:
             drawer.line((start_x, start_y, end_x, end_y),
                         fill=self.__get_color(data[i]), width=int(self.bar_width))
 
-        self.__write_text(drawer, 0, 'Algorithm', name)
-        self.__write_text(drawer, 1, 'Percent Sorted', '{:0.2f}%'.format(data.sortedness()))
-        self.__write_text(drawer, 2, 'Array Size', data.size())
-        self.__write_text(drawer, 3, 'Array Accesses', data.accesses)
-        self.__write_text(drawer, 4, 'Array Swaps', data.swaps)
-        self.__write_text(drawer, 5, 'Array Compares', data.compares)
-        self.__write_text(drawer, 6, 'Array Moves', data.moves)
+        # self.__write_text(drawer, 0, 'Algorithm', name)
+        # self.__write_text(drawer, 1, 'Percent Sorted', '{:0.2f}%'.format(data.sortedness()))
+        # self.__write_text(drawer, 2, 'Array Size', data.size())
+        # self.__write_text(drawer, 3, 'Array Accesses', data.accesses)
+        # self.__write_text(drawer, 4, 'Array Swaps', data.swaps)
+        # self.__write_text(drawer, 5, 'Array Compares', data.compares)
+        # self.__write_text(drawer, 6, 'Array Moves', data.moves)
 
         return numpy.array(img)
