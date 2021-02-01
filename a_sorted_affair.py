44970a9abe8a340cacfcee5e425ff2329cebb172
Jason Constam <callmebob@gmail.com>
2021-01-31 20:57:47 -0700

    Added ability to sort different kinds of data Added reversed data.

--- a_sorted_affair.py
+++ a_sorted_affair.py
@@ -4,6 +4,8 @@ import os
 import random
 import argparse
 
+from enum import Enum
+
 from cv2 import VideoWriter, VideoWriter_fourcc
 
 from sort_util.image_tools import draw_image
@@ -12,25 +14,43 @@ from sort_util.data_tools import data_store
 from sorters.sort import sort
 from sorters.sort_base import sort_base
 
+class data_type(Enum):
+    random = 1,
+    reverse = 2
 
-def make_video(folder: str, sorter: sort_base, rand_data: list, width: int, height: int, fps: int) -> None:
-    size = len(rand_data)
+data_types = {
+    'random': data_type.random,
+    'reverse': data_type.reverse
+}
+
+
+def make_video(folder: str, sorter: sort_base, data_type_name: str, data: list, width: int, height: int, fps: int) -> None:
+    size = len(data)
 
     drawer = draw_image(width, height, size)
     raw_file_name = os.path.join(
-        folder, 'sorted_{}_{}.avi'.format(size, sorter.name()))
+        folder, '{}_{}_{}.avi'.format(sorter.name(), size, data_type_name))
     final_file_name = os.path.join(
-        folder, 'sorted_{}_{}.mkv'.format(size, sorter.name()))
+        folder, '{}_{}_{}.mkv'.format(sorter.name(), size, data_type_name))
     video = VideoWriter(raw_file_name, VideoWriter_fourcc(*'mp4v'), float(fps),
                         drawer.get_image_size())
 
     store = data_store(drawer, video)
-    store.load(rand_data, sorter.name())
+    store.load(data, sorter.name())
     sorter.sort(store)
 
     video.release()
 
     store.convert(raw_file_name, final_file_name)
+
+def get_data(data_type_name: str, size: int):
+    data_type = data_types[data_type_name]
+    if data_type == data_type.random:
+        return random.sample(range(0, size), size)
+    elif data_type == data_type.reverse:
+        return list(range(size - 1, -1, -1))
+    else:
+        return None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -45,6 +65,8 @@ if __name__ == '__main__':
                         default=1080, help='The height of the video in pixels')
     parser.add_argument('-f', '--fps', type=int,
                         default=60, help='The framerate of the video in frames per second')
+    parser.add_argument('-d', '--dataType', type=str,
+                        default='random', choices=data_types.keys(), help='The type of data to sort')
     parser.add_argument('algorithms', metavar='alg', type=str,
                         nargs='+', help='Sorting algorithms to run')
     args = parser.parse_args()
@@ -73,6 +95,6 @@ if __name__ == '__main__':
         else:
             raise Exception('Algorithm {} not found'.format(alg))
 
-    rand_data = random.sample(range(0, args.size), args.size)
+    data = get_data(args.dataType, args.size)
     for sorter in sorters:
-        make_video(folder, sorter, rand_data.copy(), args.width, args.height, args.fps)
+        make_video(folder, sorter, args.dataType, data.copy(), args.width, args.height, args.fps)
